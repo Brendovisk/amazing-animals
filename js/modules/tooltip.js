f77f9f87d20913d706c9049649f907cd8d328d5a
Brendon <brendon.franco@vibryt.com.br>
2023-02-01 21:34:53 -0300

    feat: add a logic to make the tooltip not overflow the window

--- js/modules/tooltip.js
+++ js/modules/tooltip.js
@@ -8,11 +8,19 @@ export default class Tooltip {
     this.onMouseOver = this.onMouseOver.bind(this);
   }
 
+  // Move the tooltip according to mouse's position
   onMouseMove(event) {
+    if (event.pageX + 220 > window.innerWidth) {
+      this.tooltipBox.style.left = "unset";
+      this.tooltipBox.style.right = "20px";
+    } else {
+      this.tooltipBox.style.left = `${event.pageX + 20}px`;
+    }
+
     this.tooltipBox.style.top = `${event.pageY + 20}px`;
-    this.tooltipBox.style.left = `${event.pageX + 20}px`;
   }
 
+  // Remove the tooltip based on the mouseMove and mouseLeave
   onMouseLeave({ currentTarget }) {
     this.tooltipBox.remove();
     currentTarget.removeEventListener("mouseleave", this.onMouseLeave);
@@ -29,18 +37,21 @@ export default class Tooltip {
     this.tooltipBox = tooltipBox;
   }
 
+  // Create the tooltip and add the mouseMove and mouseLeave to the target
   onMouseOver({ currentTarget }) {
     this.createTooltip(currentTarget);
     currentTarget.addEventListener("mousemove", this.onMouseMove);
     currentTarget.addEventListener("mouseleave", this.onMouseLeave);
   }
 
+  // Add the events when the user hover your mouse over the map
   addTooltipEvent() {
     this.tooltips.forEach((item) => {
       item.addEventListener("mouseover", this.onMouseOver);
     });
   }
 
+  // Initialize
   init() {
     if (this.tooltips.length) {
       this.addTooltipEvent();
