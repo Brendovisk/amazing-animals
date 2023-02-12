6ec188d23df95f2b7cb9cec23df7fe70cfd3a562
Brendon <brendon.franco@vibryt.com.br>
2023-02-12 18:48:40 -0300

    refactor: change the business-hours function to class, and fix a problem with the mobile-menu class

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -3,33 +3,32 @@ import outsideClick from "./outside-click";
 export default class MenuMobile {
   constructor(menuButton, menuList, events) {
     this.menuButton = document.querySelector(menuButton);
-    this.this.menuList = document.querySelector(menuList);
+    this.menuList = document.querySelector(menuList);
     this.activeClass = "active";
-    if (events === undefined) this.events = ["click", "touchstart"];
+
+    if (events === undefined) this.events = ["touchstart", "click"];
     else this.events = events;
+
     this.openMenu = this.openMenu.bind(this);
   }
 
   openMenu() {
-    this.menuButton.classList.add(this.activeClass);
     this.menuList.classList.add(this.activeClass);
+    this.menuButton.classList.add(this.activeClass);
     outsideClick(this.menuList, this.events, () => {
-      this.menuButton.classList.remove(this.activeClass);
       this.menuList.classList.remove(this.activeClass);
+      this.menuButton.classList.remove(this.activeClass);
     });
   }
 
   addMenuMobileEvents() {
-    this.events.forEach((evento) => {
-      this.menuButton.addEventListener(evento, this.openMenu);
-    });
+    this.events.forEach((evento) => this.menuButton.addEventListener(evento, this.openMenu));
   }
 
   init() {
     if (this.menuButton && this.menuList) {
       this.addMenuMobileEvents();
     }
-
     return this;
   }
 }
