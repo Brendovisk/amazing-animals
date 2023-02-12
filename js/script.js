54332303b0f18b34e01f5beaa6c607e8cc10e73c
Brendon <brendon.franco@vibryt.com.br>
2023-02-12 10:20:26 -0300

    refactor: change the structure of the fetch-bitcoin function

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
-import Accordion from "./modules/accordion";
 import initDropdown from "./modules/dropdown";
-import fetchAnimals from "./modules/fetch-animais";
-import initFetchBitcoin from "./modules/fetch-bitcoin";
 import initFuncionamento from "./modules/funcionamento";
 import initMenuMobile from "./modules/menu-mobile";
-import Modal from "./modules/modal";
 import initAnimacaoScroll from "./modules/scroll-animacao";
+
 import SmoothScroll from "./modules/smooth-scroll";
+import Accordion from "./modules/accordion";
 import TabNav from "./modules/tabNav";
+import Modal from "./modules/modal";
 import Tooltip from "./modules/tooltip";
+
+import fetchAnimals from "./modules/fetch-animais";
+import fetchBitcoin from "./modules/fetch-bitcoin";
 
 const smoothScroll = new SmoothScroll("[data-menu='smooth'] a[href^='#']");
 const accordion = new Accordion("[data-anime='accordion'] dt");
@@ -16,15 +18,16 @@ const tabNav = new TabNav("[data-tab='menu'] li", "[data-tab='content'] section"
 const modal = new Modal("[data-modal='open']", "[data-modal='close']", "[data-modal='container']");
 const tooltip = new Tooltip("[data-tooltip]");
 
-accordion.init();
 smoothScroll.init();
+accordion.init();
 tabNav.init();
 modal.init();
 tooltip.init();
 
 initDropdown();
-fetchAnimals("../../animaisapi.json", ".numeros-grid");
-initFetchBitcoin();
 initFuncionamento();
 initMenuMobile();
 initAnimacaoScroll();
+
+fetchAnimals("../../animaisapi.json", ".numeros-grid");
+fetchBitcoin("https://www.blockchain.com/ticker", ".btc-preco");
